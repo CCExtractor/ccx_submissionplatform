81f728a2f91fb218b6a685d411333ee1aaf23220
canihavesomecoffee <canihavesomecoffee@users.noreply.github.com>
2015-07-30 22:05:21 +0200

    Upgraded FTPConnector
    Upgraded to implement the interface for service provider. Reset is WIP

--- src/containers/FTPConnector.php
+++ src/containers/FTPConnector.php
@@ -5,7 +5,22 @@
  */
 namespace org\ccextractor\submissionplatform\containers;
 
-class FTPConnector
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+
+class FTPConnector implements ServiceProviderInterface
 {
 
+    /**
+     * Registers services on the given container.
+     *
+     * This method should only be used to configure services and parameters.
+     * It should not get services.
+     *
+     * @param Container $pimple An Container instance
+     */
+    public function register(Container $pimple)
+    {
+        $pimple["FTPConnector"] = $this;
+    }
 }
