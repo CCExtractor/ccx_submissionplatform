91d9a2626a9942bf2877c2d071cb98b49965b0ed
canihavesomecoffee <canihavesomecoffee@users.noreply.github.com>
2015-07-30 23:35:20 +0200

    Continuation of upload controller
    - Added start page (not fully finished yet)
    - Added FTP explanation page (not fully finished either)
    - Added link int sample info page

--- src/controllers/UploadController.php
+++ src/controllers/UploadController.php
@@ -19,6 +19,56 @@ class UploadController extends BaseController
 
     function register(App $app)
     {
-        // TODO: Implement register() method.
+        $self = $this;
+        $app->group('/upload', function () use ($self) {
+            // GET: show start of controller
+            $this->get('[/]', function ($request, $response, $args) use ($self) {
+                $self->setDefaultBaseValues($this);
+                if($this->account->isLoggedIn()){
+                    // TODO: add list of unprocessed ones
+                    return $this->view->render($response,"upload/explain.html.twig",$this->templateValues->getValues());
+                }
+                return $this->view->render($response->withStatus(403),"login-required.html.twig",$this->templateValues->getValues());
+            })->setName($self->getPageName());
+            // GET: FTP upload details
+            $this->get('/ftp', function ($request, $response, $args) use ($self) {
+                $self->setDefaultBaseValues($this);
+                if($this->account->isLoggedIn()){
+                    $this->templateValues->add("host", $this->FTPConnector->getHost());
+                    $this->templateValues->add("port", $this->FTPConnector->getPort());
+
+                    $this->templateValues->add("username", "TODO");
+                    $this->templateValues->add("password", "TODO");
+                    // TODO: finish
+                    return $this->view->render($response,"upload/explain-ftp.html.twig",$this->templateValues->getValues());
+                }
+                return $this->view->render($response->withStatus(403),"login-required.html.twig",$this->templateValues->getValues());
+            })->setName($self->getPageName().'_ftp');
+            // GET: HTTP upload
+            $this->get('/new', function ($request, $response, $args) use ($self) {
+                $self->setDefaultBaseValues($this);
+                if($this->account->isLoggedIn()){
+                    // TODO: finish
+                }
+                return $this->view->render($response->withStatus(403),"login-required.html.twig",$this->templateValues->getValues());
+            })->setName($self->getPageName().'_new');
+            // Logic for finalizing samples
+            $this->group('/process', function () use ($self){
+                $this->get('[/]', function ($request, $response, $args) use ($self) {
+                    $self->setDefaultBaseValues($this);
+                    if($this->account->isLoggedIn()){
+                        // TODO: finish
+                    }
+                    return $this->view->render($response->withStatus(403),"login-required.html.twig",$this->templateValues->getValues());
+                })->setName($self->getPageName().'_process');
+                $this->get('/{id:[0-9]+}', function ($request, $response, $args) use ($self) {
+                    $self->setDefaultBaseValues($this);
+                    if($this->account->isLoggedIn()){
+                        // TODO: finish
+                    }
+                    return $this->view->render($response->withStatus(403),"login-required.html.twig",$this->templateValues->getValues());
+                })->setName($self->getPageName().'_process_id');
+            });
+        });
     }
 }
