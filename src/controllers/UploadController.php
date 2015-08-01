d7e97d8001ce1e47461bed0283f30c9a6b05d4d1
canihavesomecoffee <canihavesomecoffee@users.noreply.github.com>
2015-08-01 20:04:30 +0200

    Added automated FileZilla generation
    Preconfigured FileZilla file is now available for FTP connections.
    Others will follow later.

--- src/controllers/UploadController.php
+++ src/controllers/UploadController.php
@@ -32,26 +32,49 @@ class UploadController extends BaseController
                 return $this->view->render($response->withStatus(403),"login-required.html.twig",$this->templateValues->getValues());
             })->setName($self->getPageName());
             // GET: FTP upload details
-            $this->get('/ftp', function ($request, $response, $args) use ($self) {
-                $self->setDefaultBaseValues($this);
-                if($this->account->isLoggedIn()){
-                    $this->templateValues->add("host", $this->FTPConnector->getHost());
-                    $this->templateValues->add("port", $this->FTPConnector->getPort());
-                    // Fetch FTP username & password for user
-                    /** @var FTPCredentials $credentials */
-                    $credentials = $this->FTPConnector->getFTPCredentialsForUser($this->account->getUser());
-                    if($credentials !== false) {
-                        $this->templateValues->add("username", $credentials->getName());
-                        $this->templateValues->add("password", $credentials->getPassword());
-                    } else {
-                        $this->templateValues->add("username", "Error...");
-                        $this->templateValues->add("password", "Please get in touch...");
+            $this->group('/ftp', function () use ($self){
+                $this->get('[/]', function ($request, $response, $args) use ($self) {
+                    $self->setDefaultBaseValues($this);
+                    if($this->account->isLoggedIn()){
+                        $this->templateValues->add("host", $this->FTPConnector->getHost());
+                        $this->templateValues->add("port", $this->FTPConnector->getPort());
+                        // Fetch FTP username & password for user
+                        /** @var FTPCredentials $credentials */
+                        $credentials = $this->FTPConnector->getFTPCredentialsForUser($this->account->getUser());
+                        if($credentials !== false) {
+                            $this->templateValues->add("username", $credentials->getName());
+                            $this->templateValues->add("password", $credentials->getPassword());
+                        } else {
+                            $this->templateValues->add("username", "Error...");
+                            $this->templateValues->add("password", "Please get in touch...");
+                        }
+                        return $this->view->render($response,"upload/explain-ftp.html.twig",$this->templateValues->getValues());
                     }
-                    // TODO: finish
-                    return $this->view->render($response,"upload/explain-ftp.html.twig",$this->templateValues->getValues());
-                }
-                return $this->view->render($response->withStatus(403),"login-required.html.twig",$this->templateValues->getValues());
-            })->setName($self->getPageName().'_ftp');
+                    return $this->view->render($response->withStatus(403),"login-required.html.twig",$this->templateValues->getValues());
+                })->setName($self->getPageName().'_ftp');
+                $this->get('/filezilla', function ($request, $response, $args) use ($self) {
+                    $self->setDefaultBaseValues($this);
+                    if($this->account->isLoggedIn()){
+                        /** @var FTPCredentials $credentials */
+                        $credentials = $this->FTPConnector->getFTPCredentialsForUser($this->account->getUser());
+                        if($credentials !== false) {
+                            $props = [
+                                "host" => $this->FTPConnector->getHost(),
+                                "port" => $this->FTPConnector->getPort(),
+                                "username" => $credentials->getName(),
+                                "password" => base64_encode($credentials->getPassword())
+                            ];
+                            // Create headers
+                            $response = $response->withHeader("Content-type","text/xml");
+                            $response = $response->withHeader("Content-Disposition",'attachment; filename="FileZilla.xml"');
+                            return $response->write($this->view->getEnvironment()->loadTemplate("upload/filezilla-template.xml")->render($props));
+                        } else {
+                            return $this->view->render($response,"upload/generation-error.html.twig",$this->templateValues->getValues());
+                        }
+                    }
+                    return $this->view->render($response->withStatus(403),"login-required.html.twig",$this->templateValues->getValues());
+                })->setName($self->getPageName().'_ftp_filezilla');
+            });
             // GET: HTTP upload
             $this->get('/new', function ($request, $response, $args) use ($self) {
                 $self->setDefaultBaseValues($this);
