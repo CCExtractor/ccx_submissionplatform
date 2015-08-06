bf4d4e366390019c6c1f4f7dd529bfa5c70d2a6f
canihavesomecoffee <canihavesomecoffee@users.noreply.github.com>
2015-08-06 21:53:32 +0200

    Rename to hash
    Renamed sha1 to hash, as it fits better

--- src/controllers/SampleInfoController.php
+++ src/controllers/SampleInfoController.php
@@ -29,14 +29,39 @@ class SampleInfoController extends BaseController
             $this->get('/{id:[0-9]+}', function ($request, $response, $args) use ($self) {
                 $self->setDefaultBaseValues($this);
                 // Fetch sample
-
-                return $this->view->render($response,'sample-info/sample-info-common.html.twig',$this->templateValues->getValues());
+                $sample = $this->database->getSampleById($args["id"]);
+                if($sample !== false){
+                    // Fetch media info
+                    $media = $this->file_handler->fetchMediaInfo($sample,true);
+                    if($media !== false){
+                        $this->templateValues->add("sample",$sample);
+                        $this->templateValues->add("media",$media);
+                        return $this->view->render($response,'sample-info/sample-info-common.html.twig',$this->templateValues->getValues());
+                    }
+                    $this->templateValues->add("error","error obtaining media info");
+                } else {
+                    $this->templateValues->add("error","invalid sample id");
+                }
+                return $this->view->render($response,'sample-info/sample-info-error.html.twig',$this->templateValues->getValues());
             })->setName($self->getPageName().'_id');
-            $this->get('/{sha1:[a-z0-9]+}', function ($request, $response, $args) use ($self) {
+            $this->get('/{hash:[a-z0-9]+}', function ($request, $response, $args) use ($self) {
                 $self->setDefaultBaseValues($this);
                 // Fetch sample
-                return $this->view->render($response,'sample-info/sample-info-common.html.twig',$this->templateValues->getValues());
-            })->setName($self->getPageName().'_sha1');
+                $sample = $this->database->getSampleByHash($args["hash"]);
+                if($sample !== false){
+                    // Fetch media info
+                    $media = $this->file_handler->fetchMediaInfo($sample,true);
+                    if($media !== false){
+                        $this->templateValues->add("sample",$sample);
+                        $this->templateValues->add("media",$media);
+                        return $this->view->render($response,'sample-info/sample-info-common.html.twig',$this->templateValues->getValues());
+                    }
+                    $this->templateValues->add("error","error obtaining media info");
+                } else {
+                    $this->templateValues->add("error","invalid sample id");
+                }
+                return $this->view->render($response,'sample-info/sample-info-error.html.twig',$this->templateValues->getValues());
+            })->setName($self->getPageName().'_hash');
         });
     }
 }
