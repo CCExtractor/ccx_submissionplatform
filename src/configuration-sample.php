fe1bb2069152c8cf905813411478764a711693b1
canihavesomecoffee <canihavesomecoffee@users.noreply.github.com>
2015-08-08 23:05:42 +0200

    Small change in sample config

--- src/configuration-sample.php
+++ src/configuration-sample.php
@@ -24,6 +24,6 @@ define("HMAC_KEY","a_random_string_of_characters");
 define("AMAZON_SES_USER","Amazon_User_Id");
 define("AMAZON_SES_PASS","Amazon_User_Password");
 // Location to store media samples for temporary storage (for processing)
-define("TEMP_STORAGE","/path/to/temporary/storage");
+define("TEMP_STORAGE","/path/to/temporary/storage/");
 // Location to store media samples after submitting
-define("PERM_STORAGE","/path/to/permanent/storage");
+define("PERM_STORAGE","/path/to/permanent/storage/");
