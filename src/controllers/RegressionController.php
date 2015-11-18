59018619905b903e723487c9897b23b490039bba
canihavesomecoffee <canihavesomecoffee@users.noreply.github.com>
2015-11-18 21:03:08 +0100

    updates to regression test view
    - adds ajax processing for individual files, update & edit not supported
    yet
    - delete, edit of the regression test itself not supported yet either

--- src/controllers/RegressionController.php
+++ src/controllers/RegressionController.php
@@ -2,6 +2,7 @@
 namespace org\ccextractor\submissionplatform\controllers;
 
 use org\ccextractor\submissionplatform\containers\DatabaseLayer;
+use org\ccextractor\submissionplatform\containers\FileHandler;
 use org\ccextractor\submissionplatform\objects\NoticeType;
 use org\ccextractor\submissionplatform\objects\RegressionCategory;
 use org\ccextractor\submissionplatform\objects\RegressionInputType;
@@ -10,6 +11,7 @@ use org\ccextractor\submissionplatform\objects\RegressionTest;
 use Slim\App;
 use Slim\Http\Request;
 use Slim\Http\Response;
+use SplFileInfo;
 
 /**
  * Class RegressionController holds the logic for regression tests.
@@ -63,6 +65,7 @@ class RegressionController extends BaseController
                     $test = $dba->getRegression()->getRegressionTest($args["id"]);
                     if ($test->getId() > 0) {
                         $this->templateValues->add("test", $test);
+                        $self->setCSRF($this, $request);
 
                         return $this->view->render($response, "regression/test-view.html.twig",
                             $this->templateValues->getValues()
@@ -118,17 +121,157 @@ class RegressionController extends BaseController
                     // TODO: finish
                 }
                 )->setName($self->getPageName() . "_id_edit");
-                // POST: edit results (AJAX call)
+                // POST: edit/update/delete results
                 $this->post('/results', function ($request, $response, $args) use ($self) {
                     /** @var App $this */
+                    /** @var Request $request */
                     /** @var Response $response */
                     $self->setDefaultBaseValues($this);
+                    /** @var DatabaseLayer $dba */
+                    $dba = $this->database;
                     if (!$this->account->getUser()->hasRole("Contributor")) {
-                        return $this->view->render($response->withStatus(403), "forbidden.html.twig",
-                            $this->templateValues->getValues()
-                        );
+                        return $response->withJson(["error" => "You cannot perform this action."])->withStatus(403);
                     }
-                    // TODO: finish
+                    $nameKey = $this->csrf->getTokenNameKey();
+                    $valueKey = $this->csrf->getTokenValueKey();
+                    $result = [
+                        "error" => "CSRF failed",
+                        "csrf_name_key" => $nameKey,
+                        "csrf_name_value" => $request->getAttribute($nameKey),
+                        "csrf_value_key" => $valueKey,
+                        "csrf_value_value" => $request->getAttribute($valueKey)
+                    ];
+                    if($request->getAttribute('csrf_status',true) !== true){
+                        return $response->withJson($result)->withStatus(400);
+                    }
+                    $result["error"] = "An unexpected error happened.";
+                    /** @var RegressionTest $test */
+                    $test = $dba->getRegression()->getRegressionTest($args["id"]);
+                    if ($test->getId() === 0) {
+                        $result["error"] = "Invalid regression test";
+                        return $response->withJson($result)->withStatus(404);
+                    }
+
+                    // Process request
+                    if(isset($_POST["action"])){
+                        switch($_POST["action"]){
+                            case "add":
+                                // File, extra, extension, ignore
+                                if(isset($_FILES["correct"]) && isset($_POST["extra"]) && isset($_POST["extension"]) && isset($_POST["ignore"])){
+                                    // Perform some checks
+                                    // Validate string lengths
+                                    if(strlen($_POST["extra"]) === 0 || strlen($_POST["extension"]) === 0) {
+                                        $result["error"] = "Values cannot be empty";
+                                    } else {
+                                        /** @var FileHandler $fh */
+                                        $fh = $this->file_handler;
+                                        $fileCheck = $fh->isValidCorrectFile($_FILES["correct"], $_POST["extension"]);
+                                        if ($fileCheck["passed"]) {
+                                            $movedFile = $fh->addCorrectResult($fileCheck["file"], $_POST["extension"]);
+                                            if($movedFile !== null){
+                                                if($dba->getRegression()->addRegressionTestCorrectResult($test,
+                                                    $movedFile->getBasename($_POST["extension"]), $_POST["extension"],
+                                                    $_POST["extra"], $_POST["ignore"])){
+                                                    $result = ["status" => "Added"];
+                                                } else {
+                                                    $result["error"] = "Could not upload to the database";
+                                                }
+                                            } else {
+                                                $result["error"] = "File could not be moved";
+                                            }
+                                        } else {
+                                            $result["error"] = "File was not uploaded correctly: ".$fileCheck["file"];
+                                        }
+                                    }
+                                } else {
+                                    $result["error"] = "Not all values are filled in.";
+                                }
+                                break;
+                            case "edit":
+                                // Id, extra, extension, ignore
+                                if(isset($_POST["id"]) && isset($_POST["extra"]) && isset($_POST["extension"]) && isset($_POST["ignore"])){
+                                    // Validate string lengths
+                                    if(strlen($_POST["extra"]) === 0 || strlen($_POST["extension"]) === 0){
+                                        $result["error"] = "Values cannot be empty";
+                                    } else {
+                                        // Validate id
+                                        $r = $dba->getRegression()->getOutputTestResultById($_POST["id"]);
+                                        if($r !== null){
+                                            if($dba->getRegression()->editRegressionTestCorrectResult($r,
+                                                $_POST["extension"], $_POST["extra"], $_POST["ignore"]
+                                            )){
+                                                $result = ["status" => "Edited"];
+                                            } else {
+                                                $result["error"] = "Could not upload to the database";
+                                            }
+                                        } else {
+                                            $result["error"] = "Invalid result id";
+                                        }
+                                    }
+                                } else {
+                                    $result["error"] = "Not all values are filled in.";
+                                }
+                                break;
+                            case "update":
+                                // Id, file
+                                if(isset($_POST["id"]) && isset($_FILES["correct"])){
+                                    // Validate id
+                                    $r = $dba->getRegression()->getOutputTestResultById($_POST["id"]);
+                                    if($r !== null){
+                                        /** @var FileHandler $fh */
+                                        $fh = $this->file_handler;
+                                        $fileCheck = $fh->isValidCorrectFile($_FILES["correct"], $r->getCorrectExtension());
+                                        if($fileCheck["passed"]){
+                                            $movedFile = $fh->addCorrectResult($fileCheck["file"], $_POST["extension"]);
+                                            if($movedFile !== null){
+                                                if($dba->getRegression()->updateRegressionTestCorrectResult($r,
+                                                    $movedFile->getBasename($r->getCorrectExtension()))){
+                                                    $result = ["status" => "Updated"];
+                                                } else {
+                                                    $result["error"] = "Could not upload to the database";
+                                                }
+                                            } else {
+                                                $result["error"] = "File could not be moved";
+                                            }
+                                        } else {
+                                            $result["error"] = "File was not uploaded correctly: ".$fileCheck["file"];
+                                        }
+                                    } else {
+                                        $result["error"] = "Invalid result id";
+                                    }
+                                } else {
+                                    $result["error"] = "Not all values are filled in.";
+                                }
+                                break;
+                            case "delete":
+                                // Id
+                                if(isset($_POST["id"])){
+                                    // Validate id
+                                    $result = $dba->getRegression()->getOutputTestResultById($_POST["id"]);
+                                    if($result !== null){
+                                        if($dba->getRegression()->deleteOutputTestResult($result)){
+                                            $result = ["ok" => "Deleted"];
+                                        } else {
+                                            $result["error"] = "Could not delete from database";
+                                        }
+                                    }
+                                } else {
+                                    $result["error"] = "Not all values are filled in.";
+                                }
+                                break;
+                            default:
+                                $result["error"] = "Invalid action.";
+                                break;
+                        }
+                    } else {
+                        $result["error"] = "Invalid action.";
+                    }
+                    // Return result
+                    $response = $response->withJson($result);
+                    if(array_key_exists("error",$result)){
+                        $response = $response->withStatus(400);
+                    }
+                    return $response;
                 }
                 )->setName($self->getPageName() . "_id_results");
             }
--- src/dbal/RegressionDBAL.php
+++ src/dbal/RegressionDBAL.php
@@ -15,7 +15,7 @@ use PDOException;
  *
  * @package org\ccextractor\submissionplatform\dbal
  */
-class RegressionDBAL extends  AbstractDBAL
+class RegressionDBAL extends AbstractDBAL
 {
     public function getAllRegressionTests()
     {
@@ -30,7 +30,8 @@ FROM regression_test r
 	JOIN sample s ON s.id = r.sample_id
 	LEFT JOIN regression_test_category z ON z.regression_test_id = r.id
 	LEFT JOIN category c ON c.id = z.category_id
-ORDER BY r.id, o.test_out_id ASC;");
+ORDER BY r.id, o.test_out_id ASC;"
+        );
         $results = [];
         if ($stmt->execute() && $stmt->rowCount() > 0) {
             $data = $stmt->fetch();
@@ -38,33 +39,36 @@ ORDER BY r.id, o.test_out_id ASC;");
             /** @var RegressionTest $test */
             $test = null;
             while ($data !== false) {
-                if($id !== $data["regression_id"]){
-                    if($test !== null){
+                if ($id !== $data["regression_id"]) {
+                    if ($test !== null) {
                         $results[] = $test;
                     }
                     $test = new RegressionTest(
                         $data['regression_id'],
-                        new Sample($data['sample_id'],$data['sample_hash'],$data['sample_extension']),
-                        new RegressionCategory($data['category_id'],$data['category_name'],$data['category_description']),
+                        new Sample($data['sample_id'], $data['sample_hash'], $data['sample_extension']),
+                        new RegressionCategory($data['category_id'], $data['category_name'],
+                            $data['category_description']
+                        ),
                         $data['regression_command'],
                         RegressionInputType::fromDatabaseString($data['regression_input']),
                         RegressionOutputType::fromDatabaseString($data['regression_output'])
                     );
                     $id = $test->getId();
                 }
-                if($data['rt_id'] > 0) {
+                if ($data['rt_id'] > 0) {
                     $test->addOutputFile(new RegressionTestResult(
-                        $data['rt_id'], $data['rt_correct'], $data['rt_correct_extension'], $data['rt_extra'],
-                        $data['rt_ignore']
-                    )
+                            $data['rt_id'], $data['rt_correct'], $data['rt_correct_extension'], $data['rt_extra'],
+                            $data['rt_ignore']
+                        )
                     );
                 }
                 $data = $stmt->fetch();
             }
-            if($test !== null){
+            if ($test !== null) {
                 $results[] = $test;
             }
         }
+
         return $results;
     }
 
@@ -72,13 +76,14 @@ ORDER BY r.id, o.test_out_id ASC;");
     {
         $stmt = $this->pdo->prepare("SELECT * FROM category ORDER BY name ASC");
         $result = [];
-        if($stmt->execute() && $stmt->rowCount() > 0){
+        if ($stmt->execute() && $stmt->rowCount() > 0) {
             $data = $stmt->fetch();
-            while($data !== false){
-                $result[] = new RegressionCategory($data['id'],$data['name'],$data['description']);
+            while ($data !== false) {
+                $result[] = new RegressionCategory($data['id'], $data['name'], $data['description']);
                 $data = $stmt->fetch();
             }
         }
+
         return $result;
     }
 
@@ -96,8 +101,9 @@ FROM regression_test r
 	LEFT JOIN regression_test_category z ON z.regression_test_id = r.id
 	LEFT JOIN category c ON c.id = z.category_id
 WHERE r.id = :id
-ORDER BY r.id, o.test_out_id ASC;");
-        $stmt->bindParam(":id",$id, PDO::PARAM_INT);
+ORDER BY r.id, o.test_out_id ASC;"
+        );
+        $stmt->bindParam(":id", $id, PDO::PARAM_INT);
         $result = RegressionTest::getNullInstance();
         if ($stmt->execute() && $stmt->rowCount() > 0) {
             $data = $stmt->fetch();
@@ -105,37 +111,40 @@ ORDER BY r.id, o.test_out_id ASC;");
             /** @var RegressionTest $test */
             $test = null;
             while ($data !== false) {
-                if($id !== $data["regression_id"]){
+                if ($id !== $data["regression_id"]) {
                     $test = new RegressionTest(
                         $data['regression_id'],
-                        new Sample($data['sample_id'],$data['sample_hash'],$data['sample_extension']),
-                        new RegressionCategory($data['category_id'],$data['category_name'],$data['category_description']),
+                        new Sample($data['sample_id'], $data['sample_hash'], $data['sample_extension']),
+                        new RegressionCategory($data['category_id'], $data['category_name'],
+                            $data['category_description']
+                        ),
                         $data['regression_command'],
                         RegressionInputType::fromDatabaseString($data['regression_input']),
                         RegressionOutputType::fromDatabaseString($data['regression_output'])
                     );
                     $id = $test->getId();
                 }
-                if($data['rt_id'] > 0) {
+                if ($data['rt_id'] > 0) {
                     $test->addOutputFile(new RegressionTestResult(
-                        $data['rt_id'], $data['rt_correct'], $data['rt_correct_extension'], $data['rt_extra'],
-                        $data['rt_ignore']
-                    )
+                            $data['rt_id'], $data['rt_correct'], $data['rt_correct_extension'], $data['rt_extra'],
+                            $data['rt_ignore']
+                        )
                     );
                 }
                 $data = $stmt->fetch();
             }
-            if($test !== null){
+            if ($test !== null) {
                 $result = $test;
             }
         }
+
         return $result;
     }
 
     public function addCategory($name, $description)
     {
         $stmt = $this->pdo->prepare("INSERT INTO category VALUES (NULL, :name, :description);");
-        $stmt->bindParam(":name",$name, PDO::PARAM_STR);
+        $stmt->bindParam(":name", $name, PDO::PARAM_STR);
         $stmt->bindParam(":description", $description, PDO::PARAM_STR);
 
         return ($stmt->execute() && $stmt->rowCount() === 1);
@@ -149,12 +158,13 @@ ORDER BY r.id, o.test_out_id ASC;");
     public function getCategory($id)
     {
         $stmt = $this->pdo->prepare("SELECT * FROM category WHERE id = :id LIMIT 1;");
-        $stmt->bindParam(":id",$id, PDO::PARAM_INT);
+        $stmt->bindParam(":id", $id, PDO::PARAM_INT);
         $result = RegressionCategory::getNullInstance();
-        if($stmt->execute() && $stmt->rowCount() == 1){
+        if ($stmt->execute() && $stmt->rowCount() == 1) {
             $data = $stmt->fetch();
-            $result = new RegressionCategory($data["id"],$data["name"],$data["description"]);
+            $result = new RegressionCategory($data["id"], $data["name"], $data["description"]);
         }
+
         return $result;
     }
 
@@ -172,8 +182,9 @@ FROM regression_test r
 	LEFT JOIN regression_test_category z ON z.regression_test_id = r.id
 	LEFT JOIN category c ON c.id = z.category_id
 WHERE c.id = :id
-ORDER BY r.id, o.test_out_id ASC;");
-        $stmt->bindParam(":id",$id, PDO::PARAM_INT);
+ORDER BY r.id, o.test_out_id ASC;"
+        );
+        $stmt->bindParam(":id", $id, PDO::PARAM_INT);
         $results = [];
         if ($stmt->execute() && $stmt->rowCount() > 0) {
             $data = $stmt->fetch();
@@ -181,13 +192,13 @@ ORDER BY r.id, o.test_out_id ASC;");
             /** @var RegressionTest $test */
             $test = null;
             while ($data !== false) {
-                if($id !== $data["regression_id"]){
-                    if($test !== null){
+                if ($id !== $data["regression_id"]) {
+                    if ($test !== null) {
                         $results[] = $test;
                     }
                     $test = new RegressionTest(
                         $data['regression_id'],
-                        new Sample($data['sample_id'],$data['sample_hash'],$data['sample_extension']),
+                        new Sample($data['sample_id'], $data['sample_hash'], $data['sample_extension']),
                         $category,
                         $data['regression_command'],
                         RegressionInputType::fromDatabaseString($data['regression_input']),
@@ -195,19 +206,20 @@ ORDER BY r.id, o.test_out_id ASC;");
                     );
                     $id = $test->getId();
                 }
-                if($data['rt_id'] > 0) {
+                if ($data['rt_id'] > 0) {
                     $test->addOutputFile(new RegressionTestResult(
-                        $data['rt_id'], $data['rt_correct'], $data['rt_correct_extension'], $data['rt_extra'],
-                        $data['rt_ignore']
-                    )
+                            $data['rt_id'], $data['rt_correct'], $data['rt_correct_extension'], $data['rt_extra'],
+                            $data['rt_ignore']
+                        )
                     );
                 }
                 $data = $stmt->fetch();
             }
-            if($test !== null){
+            if ($test !== null) {
                 $results[] = $test;
             }
         }
+
         return $results;
     }
 
@@ -215,7 +227,8 @@ ORDER BY r.id, o.test_out_id ASC;");
     {
         $id = $category->getId();
         $stmt = $this->pdo->prepare("DELETE FROM category WHERE id = :id LIMIT 1");
-        $stmt->bindParam(":id",$id, PDO::PARAM_INT);
+        $stmt->bindParam(":id", $id, PDO::PARAM_INT);
+
         return $stmt->execute() && $stmt->rowCount() === 1;
     }
 
@@ -225,7 +238,7 @@ ORDER BY r.id, o.test_out_id ASC;");
         $name = $category->getName();
         $description = $category->getDescription();
         $stmt = $this->pdo->prepare("UPDATE category SET name = :name, description = :description WHERE id = :id");
-        $stmt->bindParam(":id",$id, PDO::PARAM_INT);
+        $stmt->bindParam(":id", $id, PDO::PARAM_INT);
         $stmt->bindParam(":name", $name, PDO::PARAM_STR);
         $stmt->bindParam(":description", $description, PDO::PARAM_STR);
 
@@ -244,15 +257,15 @@ ORDER BY r.id, o.test_out_id ASC;");
         $command = $test->getCommand();
         $inputType = $test->getInput()->toDatabaseString();
         $outputType = $test->getOutput()->toDatabaseString();
-
-        if($this->pdo->beginTransaction()){
+        if ($this->pdo->beginTransaction()) {
             try {
-                $stmt = $this->pdo->prepare("INSERT INTO regression_test VALUES (NULL, :sample, :command, :input, :output);");
-                $stmt->bindParam(":sample",$sampleId, PDO::PARAM_INT);
+                $stmt = $this->pdo->prepare("INSERT INTO regression_test VALUES (NULL, :sample, :command, :input, :output);"
+                );
+                $stmt->bindParam(":sample", $sampleId, PDO::PARAM_INT);
                 $stmt->bindParam(":command", $command, PDO::PARAM_STR);
                 $stmt->bindParam(":input", $inputType, PDO::PARAM_STR);
                 $stmt->bindParam(":output", $outputType, PDO::PARAM_STR);
-                if($stmt->execute()){
+                if ($stmt->execute()) {
                     $id = $this->pdo->lastInsertId();
                     $stmt = $this->pdo->prepare("INSERT INTO regression_test_category VALUES (:category, :id);");
                     $stmt->bindParam(":id", $id, PDO::PARAM_INT);
@@ -261,11 +274,80 @@ ORDER BY r.id, o.test_out_id ASC;");
                     $this->pdo->commit();
                     $test->setId($id);
                 }
-            } catch(PDOException $p){
+            } catch (PDOException $p) {
                 $this->pdo->rollBack();
             }
         }
 
         return $test;
+    }
+
+    public function getOutputTestResultById($id)
+    {
+        $stmt = $this->pdo->prepare("SELECT * FROM regression_test_out WHERE test_out_id = :id LIMIT 1;");
+        $stmt->bindParam(":id", $id, PDO::PARAM_INT);
+        $result = null;
+        if ($stmt->execute() && $stmt->rowCount() === 1) {
+            $data = $stmt->fetch();
+            $result = new RegressionTestResult($id, $data["correct"], $data["correct_extension"],
+                $data["expected_filename"], $data["ignore"]
+            );
+        }
+
+        return $result;
+    }
+
+    public function addRegressionTestCorrectResult(
+        RegressionTest $test,
+        $correct,
+        $correct_extension,
+        $expected_filename,
+        $ignore
+    ) {
+        $id = $test->getId();
+        $stmt = $this->pdo->prepare("INSERT INTO regression_test_out VALUES (NULL, :id, :correct, :correct_extension, :expected_filename, :ignore);"
+        );
+        $stmt->bindParam(":id", $id, PDO::PARAM_INT);
+        $stmt->bindParam(":correct", $correct, PDO::PARAM_STR);
+        $stmt->bindParam(":correct_extension", $correct_extension, PDO::PARAM_STR);
+        $stmt->bindParam(":expected_filename", $expected_filename, PDO::PARAM_STR);
+        $stmt->bindParam(":ignore", $ignore, PDO::PARAM_BOOL);
+
+        return $stmt->execute() && $stmt->rowCount() === 1;
+    }
+
+    public function deleteOutputTestResult(RegressionTestResult $result)
+    {
+        $id = $result->getId();
+        $stmt = $this->pdo->prepare("DELETE FROM regression_test_out WHERE test_out_id = :id LIMIT 1;");
+        $stmt->bindParam(":id", $id, PDO::PARAM_INT);
+
+        return $stmt->execute() && $stmt->rowCount() === 1;
+    }
+
+    public function updateRegressionTestCorrectResult(RegressionTestResult $result, $newCorrect)
+    {
+        $id = $result->getId();
+        $stmt = $this->pdo->prepare("UPDATE regression_test_out SET correct = :correct WHERE test_out_id = :id");
+        $stmt->bindParam(":id", $id, PDO::PARAM_INT);
+        $stmt->bindParam(":correct", $newCorrect, PDO::PARAM_STR);
+
+        return $stmt->execute() && $stmt->rowCount() === 1;
+    }
+
+    public function editRegressionTestCorrectResult(
+        RegressionTestResult $result,
+        $correct_extension,
+        $expected_filename,
+        $ignore
+    ) {
+        $id = $result->getId();
+        $stmt = $this->pdo->prepare("UPDATE regression_test_out SET correct_extension = :extension, expected_filename = :expected, `ignore` = :ignore WHERE test_out_id = :id");
+        $stmt->bindParam(":id", $id, PDO::PARAM_INT);
+        $stmt->bindParam(":extension", $correct_extension, PDO::PARAM_STR);
+        $stmt->bindParam(":expected", $expected_filename, PDO::PARAM_STR);
+        $stmt->bindParam(":ignore", $ignore, PDO::PARAM_BOOL);
+
+        return $stmt->execute() && $stmt->rowCount() === 1;
     }
 }
